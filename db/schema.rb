db09ac8a840143e5062826c84d8128154b791c8d
eiske fetchly <eiske@fetch.ly>
2025-01-09 10:25:29 -0300

    module-6

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_08_212144) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_09_131323) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "rating"
     t.decimal "total_gross"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
+    t.date "released_on"
   end
 
 end
